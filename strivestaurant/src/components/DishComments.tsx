6d2225f0d6aa20f561b7dc5507a02ed3c01af4a8
Sanaz M <sanazmohammaddi@gmail.com>
2021-12-02 15:24:34 +0000

    need more changes

---
+++ strivestaurant/src/components/DishComments.tsx
@@ -0,0 +1,19 @@
+import ListGroup from 'react-bootstrap/ListGroup'
+import IPasta from '../interfaces/pasta'
+
+interface pastaComment {
+  selectedDish: IPasta
+}
+const DishComments = ({ selectedDish }: pastaComment) => (
+  <ListGroup>
+    {selectedDish ? (
+      selectedDish.comments.map((c) => (
+        <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+      ))
+    ) : (
+      <ListGroup.Item>Click on a pasta to see the reviews!</ListGroup.Item>
+    )}
+  </ListGroup>
+)
+
+export default DishComments
